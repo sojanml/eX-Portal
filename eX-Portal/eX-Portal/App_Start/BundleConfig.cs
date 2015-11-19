a1a6eeb371bc0c7512ddefa91394a54c09dbc56e
sojanml <sojanml@gmail.com>
2015-11-19 11:14:22 +0400

    Layout Updated

--- eX-Portal/eX-Portal/App_Start/BundleConfig.cs
+++ eX-Portal/eX-Portal/App_Start/BundleConfig.cs
@@ -24,7 +24,6 @@ namespace eX_Portal
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
     }
