81619585b635b49d4604fc6268542ebbc67238fe
ajithBhaskaran <ajithbhaskaranmail@gmail.com>
2015-11-22 22:59:30 +0400

    login

--- eX-Portal/eX-Portal/Controllers/UserLoginController.cs
+++ eX-Portal/eX-Portal/Controllers/UserLoginController.cs
@@ -9,31 +9,34 @@ namespace eX_Portal.Controllers
     public class UserLoginController : Controller
     {
         // GET: UserLogin
-        public ActionResult Index()
+        public ActionResult Login()
         {
             UserLogin _objuserlogin = new UserLogin();
             return View(_objuserlogin);
         }
         [HttpPost]
-        public ActionResult Index(UserLogin _objuserlogin)
+        public ActionResult Login(UserLogin _objuserlogin)
         {
             /*Create instance of entity model*/
-          
+             ExponentPortalEntities objentity = new ExponentPortalEntities();
             /*Getting data from database for user validation*/
-            var _objuserdetail = (from data in objentity.UserDetails
-                                  where data.UserId == _objuserloginmodel.UserId
-                                  && data.Password == _objuserloginmodel.Password
+
+            var _objuserdetail = (from data in objentity.MSTR_User
+                                  where data.UserName == _objuserlogin.UserName                                  
+                                  && data.Password == _objuserlogin.Password
                                   select data);
             if (_objuserdetail.Count() > 0)
             {
                 /*Redirect user to success apge after successfull login*/
                 ViewBag.Message = 1;
+                return RedirectToAction("Index", "Home");
+
             }
             else
             {
                 ViewBag.Message = 0;
             }
-            return View(objuserloginmodel);
+            return View(_objuserlogin);
         }
     }
 }
