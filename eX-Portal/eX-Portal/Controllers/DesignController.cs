a8d5c7a90fea62d074a341738831d1c6a02a768f
ajithBhaskaran <ajithbhaskaranmail@gmail.com>
2015-11-25 11:15:56 +0400

    design controller

--- eX-Portal/eX-Portal/Controllers/DesignController.cs
+++ eX-Portal/eX-Portal/Controllers/DesignController.cs
@@ -4,18 +4,31 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using eX_Portal.Models;
+using eX_Portal.ViewModel;
 namespace eX_Portal.Controllers
 {
     public class DesignController : Controller
     {
         // GET: Design
 
+
         public DesignController()
         {
             ViewBag.Menu = BuildMenu();
+       
+       
+
+          
+
         }
        
 
+        public ActionResult SystemMenu()
+        {
+
+            var MenuList = BuildMenu();
+            return View(MenuList);
+        }
 
         private ExponentPortalEntities db = new ExponentPortalEntities();
         private IList<MenuModel> BuildMenu()
@@ -23,7 +36,21 @@ namespace eX_Portal.Controllers
 
             IList<MenuModel> mmList = new List<MenuModel>();
             var menu_items = db.MSTR_Menu;
-            foreach (MSTR_Menu mnu in menu_items)
+
+           
+
+          
+                var UserId = System.Web.HttpContext.Current.Session["UserId"] == null ? 0 : System.Web.HttpContext.Current.Session["UserId"];
+          
+                
+            String SQL = "select * from MSTR_Menu where MenuId" +
+                " in(select b.MenuId from  MSTR_Profile a left join   M2M_UserProfile b" +
+                 "  on a.ProfileId = b.ProfileId left join MSTR_User c on b.ProfileId = c.UserProfileId" +
+                 "   where c.UserId ="+ UserId + ")";
+          var studentName = db.Database.SqlQuery<MSTR_Menu>(SQL);
+          
+
+            foreach (MSTR_Menu mnu in studentName)
             {
 
                 MenuModel model = new MenuModel();
@@ -32,6 +59,8 @@ namespace eX_Portal.Controllers
                 model.Name = mnu.MenuName;
                 model.ParentId = mnu.ParentId.GetValueOrDefault();
                 model.SortOrder = mnu.SortOrder.GetValueOrDefault();
+                model.PageUrl = mnu.PageUrl;
+
                 mmList.Add(model);
             }
 
