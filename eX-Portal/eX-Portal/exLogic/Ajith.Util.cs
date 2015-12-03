800d224eb85301ffe8073d2f7ce1986c2498d8c1
ajithBhaskaran <ajithbhaskaranmail@gmail.com>
2015-12-03 15:09:13 +0400

    util1 to util

--- eX-Portal/eX-Portal/exLogic/Ajith.Util.cs
+++ eX-Portal/eX-Portal/exLogic/Ajith.Util.cs
@@ -9,168 +9,171 @@ using System.Web.Mvc;
 using System.Data;
 using System.Data.Common;
 using System.Data.SqlClient;
-
-    public partial class Util1
+namespace eX_Portal.exLogic
 {
-
-    static IEnumerable<SelectListItem> DropDownList = Enumerable.Empty<SelectListItem>();
-    private static ExponentPortalEntities ctx;
-
-    public static Int32 toInt(String sItem)
+    public partial class Util
     {
-        int Temp;
-        if(Int32.TryParse(sItem, out Temp))
-        {
-            return Int32.Parse(sItem);
-        } else
-        {
-            return 0;
-        }
-    }
 
-    public static IEnumerable<SelectListItem> GetDropDowntList(string TypeField, string NameField, string ValueField, string SPName)
-    {
-        //  ctx=new ExponentPortalEntities();
-        List<SelectListItem> SelectList = new List<SelectListItem>();
-      
-        using (var ctx = new ExponentPortalEntities())
+        static IEnumerable<SelectListItem> DropDownLists = Enumerable.Empty<SelectListItem>();
+        private static ExponentPortalEntities cotx;
+
+        public static Int32 toInt(String sItem)
         {
-            using (var cmd = ctx.Database.Connection.CreateCommand())
+            int Temp;
+            if (Int32.TryParse(sItem, out Temp))
             {
-
-                ctx.Database.Connection.Open();
-
-
-                cmd.CommandText = "usp_Portal_DroneServiceType";
-                DbParameter Param = cmd.CreateParameter();
-                Param.ParameterName = "@Type";
-                Param.Value = TypeField;
-                cmd.Parameters.Add(Param);
-                cmd.CommandType = CommandType.StoredProcedure;
-                using (var reader = cmd.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-
-                        SelectList.Add(new SelectListItem { Text = reader["Name"].ToString(), Value = reader["Code"].ToString() });
-
-                    }
-                }
-                DropDownList = SelectList.ToList();
-                ctx.Database.Connection.Close();
-                return DropDownList; //return the list objects
-
+                return Int32.Parse(sItem);
+            }
+            else
+            {
+                return 0;
             }
         }
-    }
 
-
-    /*Populating the drop down from Table Mst_Drone using sp usp_Portal_DroneNameList */
-    public static IEnumerable<SelectListItem> DroneList(string SPName)
-    {
-        //  ctx=new ExponentPortalEntities();
-        
-        List<SelectListItem> SelectList = new List<SelectListItem>();
-        using (var ctx = new ExponentPortalEntities())
+        public static IEnumerable<SelectListItem> GetDropDowntLists(string TypeField, string NameField, string ValueField, string SPName)
         {
-            using (var cmd = ctx.Database.Connection.CreateCommand())
+            //  ctx=new ExponentPortalEntities();
+            List<SelectListItem> SelectList = new List<SelectListItem>();
+
+            using (var cotx = new ExponentPortalEntities())
             {
-
-                ctx.Database.Connection.Open();
-
-
-                cmd.CommandText = "usp_Portal_DroneNameList";
-               // DbParameter Param = cmd.CreateParameter();
-               
-              
-                //cmd.Parameters.Add(Param);
-                cmd.CommandType = CommandType.StoredProcedure;
-                using (var reader = cmd.ExecuteReader())
+                using (var cmd = cotx.Database.Connection.CreateCommand())
                 {
-                    while (reader.Read())
+
+                    cotx.Database.Connection.Open();
+
+
+                    cmd.CommandText = "usp_Portal_DroneServiceType";
+                    DbParameter Param = cmd.CreateParameter();
+                    Param.ParameterName = "@Type";
+                    Param.Value = TypeField;
+                    cmd.Parameters.Add(Param);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    using (var reader = cmd.ExecuteReader())
                     {
+                        while (reader.Read())
+                        {
 
-                        SelectList.Add(new SelectListItem { Text = reader["DroneName"].ToString(), Value = reader["DroneId"].ToString() });
+                            SelectList.Add(new SelectListItem { Text = reader["Name"].ToString(), Value = reader["Code"].ToString() });
 
+                        }
                     }
-                }
-                DropDownList = SelectList.ToList();
-                ctx.Database.Connection.Close();
-                return DropDownList; //return the list objects
+                    DropDownList = SelectList.ToList();
+                    cotx.Database.Connection.Close();
+                    return DropDownLists; //return the list objects
 
+                }
             }
         }
-    }
 
 
-    public static IEnumerable<SelectListItem> DronePartsList(string SPName)
-    {
-        //  ctx=new ExponentPortalEntities();
-        List<SelectListItem> SelectList = new List<SelectListItem>();
-        using (var ctx = new ExponentPortalEntities())
+        /*Populating the drop down from Table Mst_Drone using sp usp_Portal_DroneNameList */
+        public static IEnumerable<SelectListItem> DroneList(string SPName)
         {
-            using (var cmd = ctx.Database.Connection.CreateCommand())
+            //  ctx=new ExponentPortalEntities();
+
+            List<SelectListItem> SelectList = new List<SelectListItem>();
+            using (var cotx = new ExponentPortalEntities())
             {
-
-                ctx.Database.Connection.Open();
-
-
-                cmd.CommandText = "usp_Portal_GetDroneParts";
-                // DbParameter Param = cmd.CreateParameter();
-
-
-                //cmd.Parameters.Add(Param);
-                cmd.CommandType = CommandType.StoredProcedure;
-                using (var reader = cmd.ExecuteReader())
+                using (var cmd = cotx.Database.Connection.CreateCommand())
                 {
-                    while (reader.Read())
+
+                    cotx.Database.Connection.Open();
+
+
+                    cmd.CommandText = "usp_Portal_DroneNameList";
+                    // DbParameter Param = cmd.CreateParameter();
+
+
+                    //cmd.Parameters.Add(Param);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    using (var reader = cmd.ExecuteReader())
                     {
-                        string SupplierId = reader["SupplierId"].ToString();
+                        while (reader.Read())
+                        {
 
-                        
-                        SelectList.Add(new SelectListItem { Text = reader["PartsName"].ToString()+"::" +reader["Model"].ToString()+ "::" + GetCompanyName(SupplierId) , Value = reader["PartsId"].ToString() });
+                            SelectList.Add(new SelectListItem { Text = reader["DroneName"].ToString(), Value = reader["DroneId"].ToString() });
 
+                        }
                     }
-                }
-                DropDownList = SelectList.ToList();
-                ctx.Database.Connection.Close();
-                return DropDownList; //return the list objects
+                    DropDownLists = SelectList.ToList();
+                    cotx.Database.Connection.Close();
+                    return DropDownLists; //return the list objects
 
+                }
             }
         }
-    }
 
 
-   
-
-
-
-
-    public static int GetServiceId()
-    {
-        int result = 0;
-        using (var ctx = new ExponentPortalEntities())
+        public static IEnumerable<SelectListItem> DronePartsList(string SPName)
         {
-            String SQL = "select MAX(ServiceId)as ServiceId from MSTR_DroneService";
-            int ServiceId = ctx.Database.SqlQuery<int>(SQL).FirstOrDefault<int>();
-            result = ServiceId;
+            //  ctx=new ExponentPortalEntities();
+            List<SelectListItem> SelectList = new List<SelectListItem>();
+            using (var cotx = new ExponentPortalEntities())
+            {
+                using (var cmd = cotx.Database.Connection.CreateCommand())
+                {
+
+                    cotx.Database.Connection.Open();
+
+
+                    cmd.CommandText = "usp_Portal_GetDroneParts";
+                    // DbParameter Param = cmd.CreateParameter();
+
+
+                    //cmd.Parameters.Add(Param);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            string SupplierId = reader["SupplierId"].ToString();
+
+
+                            SelectList.Add(new SelectListItem { Text = reader["PartsName"].ToString() + "::" + reader["Model"].ToString() + "::" + GetCompanyName(SupplierId), Value = reader["PartsId"].ToString() });
+
+                        }
+                    }
+                    DropDownLists = SelectList.ToList();
+                    cotx.Database.Connection.Close();
+                    return DropDownLists; //return the list objects
+
+                }
+            }
         }
 
-        return result;
-    }
 
-    public static string GetCompanyName(string AccId)
-    {
-        string result;
-        using (var ctx = new ExponentPortalEntities())
+
+
+
+
+
+        public static int GetServiceId()
         {
-            String SQL = "select name from MSTR_Account where  AccountId="+ AccId;
-            string ServiceId = ctx.Database.SqlQuery<string>(SQL).FirstOrDefault<string>();
-            result = ServiceId;
+            int result = 0;
+            using (var cotx = new ExponentPortalEntities())
+            {
+                String SQL = "select MAX(ServiceId)as ServiceId from MSTR_DroneService";
+                int ServiceId = cotx.Database.SqlQuery<int>(SQL).FirstOrDefault<int>();
+                result = ServiceId;
+            }
+
+            return result;
         }
 
-        return result;
-    }
+        public static string GetCompanyName(string AccId)
+        {
+            string result;
+            using (var cotx = new ExponentPortalEntities())
+            {
+                String SQL = "select name from MSTR_Account where  AccountId=" + AccId;
+                string ServiceId = cotx.Database.SqlQuery<string>(SQL).FirstOrDefault<string>();
+                result = ServiceId;
+            }
 
+            return result;
+        }
+
+    }
 }
 
