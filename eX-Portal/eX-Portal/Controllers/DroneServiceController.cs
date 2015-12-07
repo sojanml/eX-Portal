7366524085cba0b4d32941d19fafb00616e14f3b
sojanml <sojanml@gmail.com>
2015-12-07 16:51:39 +0400

    Fixed the Warnings of Unused Variables in Exception and use single Handler for error.

--- eX-Portal/eX-Portal/Controllers/DroneServiceController.cs
+++ eX-Portal/eX-Portal/Controllers/DroneServiceController.cs
@@ -364,10 +364,10 @@ namespace eX_Portal.Controllers
                 }
                     return RedirectToAction("Index");
             }
-            catch(Exception e)
+            catch(Exception ex)
             {
-                
-                return View();
+        Util.ErrorHandler(ex);
+        return View();
             }
         }
 
