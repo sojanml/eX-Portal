225dfae1efbbad31eb539825d0256ee0994a5883
ajithBhaskaran <ajithbhaskaranmail@gmail.com>
2015-12-10 14:04:02 +0400

    list viewmodel

---
+++ eX-Portal/eX-Portal/ViewModel/ListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using eX_Portal.Models;
+namespace eX_Portal.ViewModel
+{
+    public class ListViewModel
+    {
+        public IEnumerable<SelectListItem> Typelist { get; set; }
+        public LUP_Drone List{get; set; }
+    }
+}
