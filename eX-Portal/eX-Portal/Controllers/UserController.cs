840ca82c0a1f9e07be8f7bcbfb823e0d3027dfd5
ajithBhaskaran <ajithbhaskaranmail@gmail.com>
2015-12-15 14:36:48 +0400

    user Controller

--- eX-Portal/eX-Portal/Controllers/UserController.cs
+++ eX-Portal/eX-Portal/Controllers/UserController.cs
@@ -98,10 +98,12 @@ namespace eX_Portal.Controllers {
         public ActionResult Edit(int id)
         {
 
+            
             var viewModel = new ViewModel.UserViewModel.LoginViewModel.UserLogon
             {
 
 
+                
                 User = db.MSTR_User.Find(id),
 
                 ProfileList = Util.GetProfileList(),
@@ -119,18 +121,26 @@ namespace eX_Portal.Controllers {
             {
                 if (ModelState.IsValid)
                 {
-                    if (Session["UserId"] == null)
-                    {
-                        Session["UserId"] = -1;
-                    }
-                    User.LastModifiedBy = Util.toInt(Session["UserID"].ToString());
-                    User.LastModifiedOn = DateTime.Now;
 
-                   string SQL = "UPDATE MSTR_USER SET UserName='" + User.UserName + "',Password='" + User.Password + "',FirstName='" + User.FirstName + "',UserProfileId=" + User.UserProfileId + " where UserId=" + User.UserId;
-                    int id = Util.doSQL(SQL);
+
                   
-                    return RedirectToAction("UserList");
-                }
+                        if (Session["UserId"] == null)
+                        {
+                            Session["UserId"] = -1;
+                        }
+                        User.LastModifiedBy = Util.toInt(Session["UserID"].ToString());
+                        User.LastModifiedOn = DateTime.Now;
+
+                        string SQL = "UPDATE MSTR_USER SET UserName='" + User.UserName +
+                             "',Password='" + User.Password + "',UserProfileId=" + User.UserProfileId + ",FirstName='" + User.FirstName +
+                             "',UserProfileId=" + User.UserProfileId + ", Remarks='" +
+                             User.Remarks + "',MobileNo='" + User.MobileNo + "',EmailId='" +
+                             User.EmailId + "' where UserId=" + User.UserId;
+                        int id = Util.doSQL(SQL);
+
+                        return RedirectToAction("UserList");
+                    }
+               
             }
             catch (Exception ex)
             {
@@ -147,20 +157,60 @@ namespace eX_Portal.Controllers {
             if (ModelState.IsValid)
             {
 
-                var password = User.Password;
-
-                if (Session["UserId"] == null)
+                if (exLogic.User.UserExist(User.UserName)>0)
                 {
-                    Session["UserId"] = -1;
+                    ViewBag.UserExists = "User Exist Please Try Another !";
+
+                    var viewModel = new ViewModel.UserViewModel.LoginViewModel.UserLogon
+                    {
+                       User = new MSTR_User(),
+
+                        ProfileList = Util.GetProfileList(),
+                       
+                        
+
+                    };
+                    return View(viewModel);
+                   
                 }
-               User.CreatedBy = Util.toInt(Session["UserID"].ToString());
-                User.CreatedOn = DateTime.Now;
-                db.MSTR_User.Add(User);
-                db.SaveChanges();
-                return RedirectToAction("UserList");
+                else
+                {
+
+                    if (Session["UserId"] == null)
+                    {
+                        Session["UserId"] = -1;
+                    }
+                    User.CreatedBy = Util.toInt(Session["UserID"].ToString());
+                    User.CreatedOn = DateTime.Now;
+                    db.MSTR_User.Add(User);
+                    db.SaveChanges();
+                    return RedirectToAction("UserList");
+                }
             }
             return View(User);
         }
+
+
+
+
+        public String Delete([Bind(Prefix = "ID")]int UserID = 0)
+        {
+            String SQL = "";
+            Response.ContentType = "text/json";
+           // if (!exLogic.User.hasAccess("USER.DELETE"))
+             //   return Util.jsonStat("ERROR", "Access Denied");
+
+            //Delete the drone from database if there is no flights are created
+           // SQL = "SELECT Count(*) FROM [DroneFlight] WHERE DroneID = " + DroneID;
+          //  if (Util.getDBInt(SQL) != 0)
+           //     return Util.jsonStat("ERROR", "You can not delete a drone with a flight attached");
+
+            SQL = "DELETE FROM [M2M_USER] WHERE UserId = " + UserID;
+            Util.doSQL(SQL);           
+
+            return Util.jsonStat("OK");
+        }
+
 
 
 
