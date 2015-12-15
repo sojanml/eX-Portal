d09f77d98512231da654fd83e895e594423ec211
ajithBhaskaran <ajithbhaskaranmail@gmail.com>
2015-12-15 18:06:36 +0400

    mstruser

--- eX-Portal/eX-Portal/Models/MSTR_User.cs
+++ eX-Portal/eX-Portal/Models/MSTR_User.cs
@@ -11,11 +11,12 @@ namespace eX_Portal.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
     public partial class MSTR_User
     {
         public int UserId { get; set; }
         public string UserName { get; set; }
+        [Required(ErrorMessage ="Password Required")]
         public string Password { get; set; }
         public Nullable<bool> RememberMe { get; set; }
         public string PhotoUrl { get; set; }
