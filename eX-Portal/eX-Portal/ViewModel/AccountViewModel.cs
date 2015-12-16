2f1fb843434bdd35955c8826b9ccff6f28967efb
ajithBhaskaran <ajithbhaskaranmail@gmail.com>
2015-12-16 14:20:09 +0400

    account view model

---
+++ eX-Portal/eX-Portal/ViewModel/AccountViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using eX_Portal.Models;
+namespace eX_Portal.ViewModel
+{
+    public class AccountViewModel
+    {
+        public MSTR_Account Account { get; set; }
+        public IEnumerable<SelectListItem> CountryList { get; set; }
+    }
+}
