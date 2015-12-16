6e00446d0942f8f095d018f9181f1b56c7e15506
ajithBhaskaran <ajithbhaskaranmail@gmail.com>
2015-12-16 15:43:14 +0400

    mstr  account

--- eX-Portal/eX-Portal/Models/MSTR_Account.cs
+++ eX-Portal/eX-Portal/Models/MSTR_Account.cs
@@ -11,10 +11,13 @@ namespace eX_Portal.Models
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
     
     public partial class MSTR_Account
     {
         public int AccountId { get; set; }
+        [Required(ErrorMessage ="Name requird")]
         public string Name { get; set; }
         public string EmailId { get; set; }
         public string MobileNo { get; set; }
@@ -32,6 +35,7 @@ namespace eX_Portal.Models
         public string Address2 { get; set; }
         public string Address3 { get; set; }
         public Nullable<int> CountryCode { get; set; }
+       [Required(ErrorMessage = "Code Required")]
         public string Code { get; set; }
         public string BinaryCode { get; set; }
     }
