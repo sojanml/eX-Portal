396a0175a14054460fa5fda8d10663210515979f
ajithBhaskaran <ajithbhaskaranmail@gmail.com>
2015-12-17 12:41:31 +0400

    validation removed

--- eX-Portal/eX-Portal/Models/MSTR_User.cs
+++ eX-Portal/eX-Portal/Models/MSTR_User.cs
@@ -14,8 +14,7 @@ namespace eX_Portal.Models
     using System.ComponentModel.DataAnnotations;
     public partial class MSTR_User
     {
-        public int UserId { get; set; }
-        [Required(ErrorMessage ="Please Enter The UserName")]
+        public int UserId { get; set; }       
         public string UserName { get; set; }
         public string Password { get; set; }
         public Nullable<bool> RememberMe { get; set; }
