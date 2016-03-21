43708ffc50c5f7fd20e76d32f1a1ede782a5acda
ajithBhaskaran <ajithbhaskaranmail@gmail.com>
2016-03-21 09:27:14 +0400

    chartviewmodel for chart

---
+++ eX-Portal/eX-Portal/ViewModel/ChartViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eX_Portal.ViewModel
+{
+    public class ChartViewModel
+    {
+        public string DroneName { set; get;}
+        public int TotalFightTime { set; get;}
+        public int CurrentFlightTime { set; get;}
+    }
+}
