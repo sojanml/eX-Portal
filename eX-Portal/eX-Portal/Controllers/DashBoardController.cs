f24238376fc058f63c0247ec2914c915f442b806
ajithBhaskaran <ajithbhaskaranmail@gmail.com>
2016-03-21 09:24:38 +0400

    dashboardcontroller

--- eX-Portal/eX-Portal/Controllers/DashBoardController.cs
+++ eX-Portal/eX-Portal/Controllers/DashBoardController.cs
@@ -5,6 +5,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+using System.Data;
+using eX_Portal.ViewModel;
+using System.Text;
+using System.IO;
+using System.Xml;
 
 namespace eX_Portal.Controllers {
   public class DashBoardController : Controller {
@@ -47,5 +53,83 @@ namespace eX_Portal.Controllers {
 
       return View(Doc);
     }
+        [System.Web.Mvc.HttpGet]
+        public void getWeatherData(string Location)
+        {
+
+            
+        Util.GetCurrentConditions("AEXX0004" );
+
+
+        }
+
+        [System.Web.Mvc.HttpGet]
+        public JsonResult getCurrentFlightChartData()
+        {
+            List<object> iData = new List<object>();
+            List<string> labels = new List<string>();
+            List<int> lst_dataItem_2 = new List<int>();
+            List<int> lst_dataItem_1 = new List<int>();
+            List<int> lst_dataItem_3 = new List<int>();
+            List<int> lst_dataItem_4 = new List<int>();
+            List<int> lst_dataItem_5 = new List<int>();
+          
+           
+            IList<ChartViewModel> ChartList = Util.getCurrentFlightChartData();
+            foreach (ChartViewModel FMD in ChartList)
+            {
+                labels.Add(FMD.DroneName);
+                lst_dataItem_1.Add(Convert.ToInt32(FMD.TotalFightTime/60));
+                lst_dataItem_2.Add(Convert.ToInt32(FMD.CurrentFlightTime/60));              
+
+
+            }
+            iData.Add(labels);
+            iData.Add(lst_dataItem_1);
+            iData.Add(lst_dataItem_2);
+            //iData.Add(lst_dataItem_3);
+            //iData.Add(lst_dataItem_4);
+            //iData.Add(lst_dataItem_5);
+
+            return Json(iData, JsonRequestBehavior.AllowGet);
+
+        }
+
+
+
+        public ActionResult Internal()
+
+        {
+            string City,Lat=null,Lng=null,woeid;
+            if (Session["Lat"]!=null)
+            {
+                Lat = Session["Lat"].ToString();
+            }
+            else
+            {
+                Lat = "25.2048";
+            }
+            if (Session["Long"]!=null)
+            {
+                Lng = Session["Long"].ToString();
+            }
+            else
+            {
+                Lng= "55.2708";
+            }
+          
+            WeatherViewModel Weather= new WeatherViewModel();
+            //getting the exact place from lat and long
+            City = Util.GetLocation(Lat, Lng);
+            //getting the woeid from yahoo api
+            woeid = Util.GetWOEID(City);
+            //getting the weather information from  woeid           
+           Weather= Util.GetCurrentConditions(woeid);
+            return View(Weather);
+        }
+
   }
+
+
+    
 }
