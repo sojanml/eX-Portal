dcafe599b3dc1a9f5788b1bd766308a572d6771f
HudhaZ <hudha.zahur@exponent-ts.com>
2016-03-22 08:57:08 +0400

    changes

--- eX-Portal/eX-Portal/Controllers/DashBoardController.cs
+++ eX-Portal/eX-Portal/Controllers/DashBoardController.cs
@@ -13,14 +13,14 @@ using System.IO;
 using System.Xml;
 
 namespace eX_Portal.Controllers {
-  public class DashBoardController : Controller {
-    // GET: DashBoard
-    public ActionResult Default() {
-      return View();
-    }
+    public class DashBoardController : Controller {
+        // GET: DashBoard
+        public ActionResult Default() {
+            return View();
+        }
 
-    public ActionResult Dewa() {
-      String SQL = @"Select 
+        public ActionResult Dewa() {
+            String SQL = @"Select 
         Max(DroneDocuments.ID) as LastID,
         DroneDocuments.DocumentType,
         Max(DroneDocuments.UploadedDate) as LastDate
@@ -43,22 +43,22 @@ namespace eX_Portal.Controllers {
         DroneDocuments.DocumentType
       ORDER BY
         LastDate";
-      var Rows = Util.getDBRows(SQL);
-      return View(Rows);
-    }
+            var Rows = Util.getDBRows(SQL);
+            return View(Rows);
+        }
 
-    public ActionResult SubSection(int DocumentID = 0) {
-      var DB = new ExponentPortalEntities();
-      var Doc = DB.DroneDocuments.Find(DocumentID);
+        public ActionResult SubSection(int DocumentID = 0) {
+            var DB = new ExponentPortalEntities();
+            var Doc = DB.DroneDocuments.Find(DocumentID);
 
-      return View(Doc);
-    }
+            return View(Doc);
+        }
         [System.Web.Mvc.HttpGet]
         public void getWeatherData(string Location)
         {
 
-            
-        Util.GetCurrentConditions("AEXX0004" );
+
+            Util.GetCurrentConditions("AEXX0004");
 
 
         }
@@ -73,14 +73,14 @@ namespace eX_Portal.Controllers {
             List<int> lst_dataItem_3 = new List<int>();
             List<int> lst_dataItem_4 = new List<int>();
             List<int> lst_dataItem_5 = new List<int>();
-          
-           
+
+
             IList<ChartViewModel> ChartList = Util.getCurrentFlightChartData();
             foreach (ChartViewModel FMD in ChartList)
             {
                 labels.Add(FMD.DroneName);
-                lst_dataItem_1.Add(Convert.ToInt32(FMD.TotalFightTime/60));
-                lst_dataItem_2.Add(Convert.ToInt32(FMD.CurrentFlightTime/60));              
+                lst_dataItem_1.Add(Convert.ToInt32(FMD.TotalFightTime / 60));
+                lst_dataItem_2.Add(Convert.ToInt32(FMD.CurrentFlightTime / 60));
 
 
             }
@@ -100,8 +100,8 @@ namespace eX_Portal.Controllers {
         public ActionResult Internal()
 
         {
-            string City,Lat=null,Lng=null,woeid;
-            if (Session["Lat"]!=null)
+            string City, Lat = null, Lng = null, woeid;
+            if (Session["Lat"] != null)
             {
                 Lat = Session["Lat"].ToString();
             }
@@ -109,27 +109,60 @@ namespace eX_Portal.Controllers {
             {
                 Lat = "25.2048";
             }
-            if (Session["Long"]!=null)
+            if (Session["Long"] != null)
             {
                 Lng = Session["Long"].ToString();
             }
             else
             {
-                Lng= "55.2708";
+                Lng = "55.2708";
             }
-          
-            WeatherViewModel Weather= new WeatherViewModel();
+
+            WeatherViewModel Weather = new WeatherViewModel();
             //getting the exact place from lat and long
             City = Util.GetLocation(Lat, Lng);
             //getting the woeid from yahoo api
             woeid = Util.GetWOEID(City);
             //getting the weather information from  woeid           
-           Weather= Util.GetCurrentConditions(woeid);
+            Weather = Util.GetCurrentConditions(woeid);
             return View(Weather);
         }
 
-  }
-
-
     
+    public ActionResult ChartDetails()
+    {
+            String SQL = @"select t.DroneId,
+                           v.DroneName,
+                            max(T.TotalFlightTime) as TotalFlightTime,
+                           CASE WHEN  max(T.TotalFlightTime) - min(k.FlightTime)IS NULL or 
+                            max(T.TotalFlightTime) - min(k.FlightTime) = 0 
+                            THEN max(T.TotalFlightTime) ELSE max(T.TotalFlightTime) - min(k.FlightTime) END as CurrentFlightTime,
+                            Count(*) Over() as _TotalRecords,
+                            t.DroneId as _PKey
+                            from MSTR_Drone v
+                            join FlightMapData t on v.DroneId = t.DroneId 
+                            left join(select u.DroneId, min(u.ReadTime) as ReadTime,
+                            max(u.TotalFlightTime) as FlightTime
+                            from FlightMapData u
+                            where
+                            convert(nvarchar(30), u.ReadTime, 120)
+                            BETWEEN DATEADD(MONTH, DATEDIFF(MONTH, 0, GETDATE()), 0) 
+                            AND  GETDATE()
+                            group by  u.DroneId )k on t.DroneId = k.DroneId
+                            group by t.DroneId,v.DroneName";
+
+
+            qView nView = new qView(SQL);
+            if (Request.IsAjaxRequest())
+            {
+                Response.ContentType = "text /javascript";
+                return PartialView("qViewData", nView);
+            }
+            else {
+                return View(nView);
+            }
+        }
+
+
+} 
 }
