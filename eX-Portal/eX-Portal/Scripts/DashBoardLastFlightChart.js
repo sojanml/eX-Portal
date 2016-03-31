0c7b973b9344d09063c6c0eed5f888169d9620b6
ajithBhaskaran <ajithbhaskaranmail@gmail.com>
2016-03-31 12:22:54 +0400

    dashboardlast script

--- eX-Portal/eX-Portal/Scripts/DashBoardLastFlightChart.js
+++ eX-Portal/eX-Portal/Scripts/DashBoardLastFlightChart.js
@@ -7,6 +7,8 @@ var DataTempStore = {
   Speed: null
 };
 
+var xLabel = new Object();
+var ToolTipTimeout = null;
 $(document).ready(function () {
 
 
@@ -24,33 +26,38 @@ $(document).ready(function () {
     var aData = reponse;
     var aLabels = aData[0];
     var aDatasets1 = aData[1];
-    var aDatasets2 = aData[2];
+    var aDatasets2 = aData[3];
+    var aLabelsShort = aData[2];
+      //long name stored in global variable
+    for (var i = 0; i < aData[0].length; i++) {
+        var name = aLabelsShort[i];
+        var value = aLabels[i];
+
+        xLabel[name] = value;
+    }
     // var aDatasets3 = aData[3];
     //var aDatasets4 = aData[4];
     //var aDatasets5 = aData[5];
 
 
     var data = {
-      labels: aLabels,
+        labels: aLabelsShort,
       datasets: [{
         label: "Total Flight Time",
-        //  fillColor: "rgba(220,220,220,0.2)",
-        strokeColor: "rgba(220,220,220,1)",
-        pointColor: "rgba(220,220,220,1)",
-        pointStrokeColor: "#fff",
-        //  pointHighlightFill: "#fff",
-        pointHighlightStroke: "rgba(220,220,220,1)",
-        fillColor: "rgba(255,102,102,0.5)",
-        strokeColor: "rgba(255,102,102,0.8)",
-        highlightFill: "rgba(255,102,102,0.75)",
-        highlightStroke: "rgba(255,102,102,1)",
+         
+       
+        fillColor: "rgba(151,187,205,0.5)",
+        strokeColor: "rgba(151,187,205,0.8)",
+        highlightFill: "rgba(151,187,205,0.75)",
+        highlightStroke: "rgba(151,187,205,1)",
+        
         data: aDatasets1
 
       }]
     };
 
     var ctx = $("#DsChart3").get(0).getContext('2d');
-    ctx.canvas.height = 300;  // setting height of canvas
+    ctx.canvas.height = 235;  // setting height of canvas
     ctx.canvas.width = 500; // setting width of canvas
     lineChart = new Chart(ctx).Bar(data, {
       //bezierCurve: true,
@@ -85,24 +92,65 @@ $(document).ready(function () {
 
       //Number - Spacing between data sets within X values
       barDatasetSpacing: 1,
-      tooltipTemplate: "<%if (label){%><%=label %>: <%}%><%= value   %>",
+
+    //  customTooltips: function (tooltip) {
+    //   customToolTip(tooltip, event);
+    //},
+
+        
+     // tooltipTemplate: _tooltipTemplate,
+    tooltipTemplate: "<%if (label){%><%=xLabel[label]%>: <%}%><%= value   %>",
       // tooltipTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].datasetLabel){%><%=datasets[i].value%><%}%></li><%}%></ul>",
       //String - A legend template
-      multiTooltipTemplate: "<%= datasetLabel %> : <%= value %>",
-      legendTemplate:
-      '<ul id=\"line-legend">\n' +
-      '  <% for (var i=0; i<datasets.length; i++){%>\n' +
-      '  <li class="active" data-label="<%=datasets[i].label%>">\n' +
-      '    <span class="legend" style=\"background-color:<%=datasets[i].strokeColor%>\">' +
-      '     <span class="icon">&#xf00c;</span>\n' +
-      '    </span>\n' +
-      '    <span><%=datasets[i].label%></span>\n' +
-      '  </li>\n' +
-      '  <%}%>\n' +
-      '</ul>'
+      multiTooltipTemplate: "<%= value %>",
+      legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
+     
     });
     var legend = lineChart.generateLegend();
     $('#map-legent').append(legend);
+  }
+
+
+
+  function _tooltipTemplate(Label) {
+     // alert(Label);
+      return
+  }
+   
+
+  function customToolTip(tooltip, e) {
+      var tooltipEl = $('#chartjs-tooltip');
+      if (ToolTipTimeout) window.clearTimeout(ToolTipTimeout);
+      if (!tooltip) {
+          ToolTipTimeout = window.setTimeout(function () {
+              $('#chartjs-tooltip').fadeOut("fast");
+          }, 100);
+          return false;
+      }
+      var HTML = xLabel[tooltip.title];
+    
+      console.log(HTML);
+      for (var i = 0; i < tooltip.labels.length; i++) {
+          HTML += "<br>\n" +
+          '<span class="icon" style="color:' + tooltip.legendColors[i].fill + '">&#xf111;</span>\n' +
+          '<span style="font-weight:bold; color:' + tooltip.legendColors[i].fill + '">' + tooltip.labels[i] + '</span>\n';
+      }
+      tooltipEl.html(HTML);
+      /*
+      console.log(
+      tooltip.title +
+      " - x: " + tooltip.x + ", y: " + tooltip.y +
+      " - xPadding: " + tooltip.xPadding + ", yPadding: " + tooltip.yPadding +
+      " - xOffset: " + tooltip.xOffset + ", yOffset: " + tooltip.yOffset
+      );
+      */
+      tooltipEl.css({
+          display: 'block',
+          opacity: 1,
+          left: tooltip.chart.canvas.offsetLeft + tooltip.x + 'px',
+          top: tooltip.chart.canvas.offsetTop + tooltip.y + 'px'
+      });
+
   }
 
   function OnErrorCall_(repo) {
