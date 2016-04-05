5a03fa3f27a108bf5085c6968086ac69beb8fd0c
sojanml <sojanml@gmail.com>
2016-04-05 10:19:51 +0400

    Checking ChartViewModel

--- eX-Portal/eX-Portal/ViewModel/ChartViewModel.cs
+++ eX-Portal/eX-Portal/ViewModel/ChartViewModel.cs
@@ -3,15 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace eX_Portal.ViewModel
-{
-    public class ChartViewModel
-    {
-        public string DroneName { set; get;}
-        public int TotalFightTime { set; get;}
-        public int CurrentFlightTime { set; get;}
-        public string PilotName { set; get; }
-        public int PilotTotalHrs { set; get; }
-        public int PilotCurrentMonthHrs { set; get; }
-    }
+namespace eX_Portal.ViewModel {
+  public class ChartViewModel {
+    public string DroneName { set; get; }
+    public int TotalFightTime { set; get; }
+    public int CurrentFlightTime { set; get; }
+    public string PilotName { set; get; }
+    public int PilotTotalHrs { set; get; }
+    public int PilotCurrentMonthHrs { set; get; }
+  }
 }
