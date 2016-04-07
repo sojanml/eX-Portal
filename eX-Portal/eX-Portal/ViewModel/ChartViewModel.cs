4b3de85b2bf5c368470bd889be4a4c93f092e385
ajithBhaskaran <ajithbhaskaranmail@gmail.com>
2016-04-07 15:33:22 +0400

    char view model changes

--- eX-Portal/eX-Portal/ViewModel/ChartViewModel.cs
+++ eX-Portal/eX-Portal/ViewModel/ChartViewModel.cs
@@ -6,11 +6,11 @@ using System.Web;
 namespace eX_Portal.ViewModel {
   public class ChartViewModel {
     public string DroneName { set; get; }
-    public int TotalFightTime { set; get; }
-    public int CurrentFlightTime { set; get; }
+    public double TotalFightTime { set; get; }
+    public double CurrentFlightTime { set; get; }
         public string PilotName { set; get; }
-        public int PilotTotalHrs { set; get; }
-        public int PilotCurrentMonthHrs { set; get; }
-        public DateTime LastFlightTime { set; get; }
+        public double PilotTotalHrs { set; get; }
+        public double PilotCurrentMonthHrs { set; get; }
+        public double LastFlightTime { set; get; }
     }
 }
