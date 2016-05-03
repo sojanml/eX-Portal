1077bc0dab478ee8a84bfc4efea9f81e02229761
blessyu16 <blessy.thomas@exponent-ts.com>
2016-05-03 20:27:30 +0400

    flight setup viewmodel

---
+++ eX-Portal/eX-Portal/ViewModel/FlightSetupViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using eX_Portal.Models;
+
+namespace eX_Portal.ViewModel
+{
+    public class FlightSetupViewModel
+    {
+        public MSTR_Drone_Setup DroneSetup { get; set; }
+        public GCA_Approval GcaApproval { get; set; }
+    }
+}
