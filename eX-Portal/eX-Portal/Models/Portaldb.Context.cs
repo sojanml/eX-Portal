a4b117ea90d41c57f6d5bdd08024e9f99f7cd82a
roshanp4 <roshanp4@gmail.com>
2016-05-15 11:42:50 +0400

    Added new table for RPAS Registration

--- eX-Portal/eX-Portal/Models/Portaldb.Context.cs
+++ eX-Portal/eX-Portal/Models/Portaldb.Context.cs
@@ -78,6 +78,7 @@ namespace eX_Portal.Models
         public virtual DbSet<PortalAlertEmail> PortalAlertEmails { get; set; }
         public virtual DbSet<PortalAlertRegistration> PortalAlertRegistrations { get; set; }
         public virtual DbSet<FlightInfo> FlightInfoes { get; set; }
+        public virtual DbSet<MSTR_RPAS_User> MSTR_RPAS_User { get; set; }
     
         public virtual ObjectResult<usp_Portal_CreateDrone_Result> usp_Portal_CreateDrone(Nullable<int> ownerID, Nullable<int> manufacturerID, Nullable<int> uAVTypeID, Nullable<System.DateTime> commissionDate)
         {
