d9ccd1d38c16ddb65b09eda951e159b4f09bb033
blessyu16 <blessy.thomas@exponent-ts.com>
2016-06-01 15:35:59 +0400

    rpasuser controller

--- eX-Portal/eX-Portal/Controllers/RpasUserController.cs
+++ eX-Portal/eX-Portal/Controllers/RpasUserController.cs
@@ -67,15 +67,24 @@ namespace eX_Portal.Controllers
         // GET: RpasUser/Create
         public ActionResult Create([Bind(Prefix = "ID")] int RPASID = 0)
         {            
-            if (!exLogic.User.hasAccess("USER.CREATE")) return RedirectToAction("NoAccess", "Home");
-
-            //ViewBag.IsPassowrdRequired = true;
+            if (!exLogic.User.hasAccess("RPASUSER.CREATE")) return RedirectToAction("NoAccess", "Home");
 
             MSTR_User EPASValues = new MSTR_User();
             if (RPASID != 0)
             {
-                ViewBag.RPASid = RPASID;
-                //ViewBag.IsPassowrdRequired = false;               
+                ViewBag.RPASid = RPASID;                
+                //ViewBag.IsPassowrdRequired = false;
+                var RPASoList = (from p in db.MSTR_RPAS_User where p.RpasId == RPASID select p).ToList();
+                EPASValues.FirstName = RPASoList[0].Name;
+                EPASValues.CountryId = Convert.ToInt16(RPASoList[0].NationalityId);
+                EPASValues.EmiratesID = RPASoList[0].EmiratesId;
+                EPASValues.EmailId = RPASoList[0].EmailId;
+                EPASValues.MobileNo = RPASoList[0].MobileNo;
+
+                //EPASValues.UserProfileId = Convert.ToInt16(7);
+                //EPASValues.Dashboard = "RPAS";
+                //EPASValues.IsActive = false;
+                //EPASValues.IsPilot = false;
             }
 
             var viewModel = new ViewModel.UserViewModel
@@ -97,7 +106,7 @@ namespace eX_Portal.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create(ViewModel.UserViewModel UserModel, int ID = 0)
         {
-            if (!exLogic.User.hasAccess("USER.CREATE")) return RedirectToAction("NoAccess", "Home");
+            if (!exLogic.User.hasAccess("RPASUSER.CREATE")) return RedirectToAction("NoAccess", "Home");
             string hdnRPASid = Request["hdnRPASid"];
             ModelState.Remove("User.AccountId");
             ModelState.Remove("User.UserProfileId");
@@ -238,7 +247,7 @@ namespace eX_Portal.Controllers
                     SQL = "update mstr_rpas_user set [Status] = 'User Created' where rpasID = " + RPASID;
                     Util.doSQL(SQL);
                 }
-                return RedirectToAction("Index", new { ID = id });
+                return RedirectToAction("UASRegister","RPAS");
 
             }
 
@@ -261,30 +270,124 @@ namespace eX_Portal.Controllers
         // GET: RpasUser/Edit/5
         public ActionResult Edit(int? id)
         {
+            if (!exLogic.User.hasAccess("RPASUSER.EDIT")) return RedirectToAction("NoAccess", "Home");
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
             MSTR_User mSTR_User = db.MSTR_User.Find(id);
+            MSTR_User_Pilot mSTR_User_Pilot = db.MSTR_User_Pilot.Find(id);
             if (mSTR_User == null)
             {
                 return HttpNotFound();
             }
-            return View(mSTR_User);
+            var viewModel = new ViewModel.UserViewModel
+            {
+                User = mSTR_User,
+                Pilot = mSTR_User_Pilot,
+                ProfileList = Util.GetProfileList(),
+                CountryList = Util.GetCountryLists("Country", "CountryName", "Code", "sp"),
+                AccountList = Util.GetAccountList(),
+                DashboardList = Util.GetDashboardLists(),
+                PermitCategoryList = Util.GetLists("RPASCategory")
+
+            };
+            return View(viewModel);
+            //return View(mSTR_User);
         }
 
         // POST: RpasUser/Edit/5     
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(MSTR_User mSTR_User)
+        public ActionResult Edit(ViewModel.UserViewModel UserModel, int ID = 0)
         {
+            if (!exLogic.User.hasAccess("RPASUSER.EDIT")) return RedirectToAction("NoAccess", "Home");
+            string hdnRPASid = Request["hdnRPASid"];
+            ModelState.Remove("User.AccountId");
+            ModelState.Remove("User.UserProfileId");
+            ModelState.Remove("User.IsActive");
+            ModelState.Remove("User.IsPilot");
+            if (String.IsNullOrEmpty(UserModel.User.RPASPermitNo)) ModelState.AddModelError("User.RPASPermitNo", "Please enter the RPAS Permit Number");
+            if (String.IsNullOrEmpty(UserModel.User.PermitCategory)) ModelState.AddModelError("User.PermitCategory", "Please select the RPAS Permit Category");
+            if (String.IsNullOrEmpty(UserModel.User.ContactAddress)) ModelState.AddModelError("User.ContactAddress", "Please enter Contact Address");
+            if (String.IsNullOrEmpty(UserModel.User.RegRPASSerialNo)) ModelState.AddModelError("User.RegRPASSerialNo", "Please enter RPAS Serial Number");
+            if (String.IsNullOrEmpty(UserModel.User.CompanyAddress)) ModelState.AddModelError("User.CompanyAddress", "Please enter Company Address");
+            if (String.IsNullOrEmpty(UserModel.User.CompanyTelephone)) ModelState.AddModelError("User.CompanyTelephone", "Please enter Company Telephone Number.");
+            if (String.IsNullOrEmpty(UserModel.User.CompanyEmail)) ModelState.AddModelError("User.CompanyEmail", "Please enter Company Email");
+            if (String.IsNullOrEmpty(UserModel.User.RPASPermitNo)) ModelState.AddModelError("User.RPASPermitNo", "Please enter the RPAS Permit Number");
+            if (String.IsNullOrEmpty(UserModel.Pilot.EmiratesId)) ModelState.AddModelError("Pilot.EmiratesId", "Emirates ID is required.");
+            if (String.IsNullOrEmpty(UserModel.Pilot.PassportNo)) ModelState.AddModelError("Pilot.PassportNo", "Passport Number is required.");
+
+            int RPASID = ID;   
+
             if (ModelState.IsValid)
-            {
-                db.Entry(mSTR_User).State = EntityState.Modified;
-                db.SaveChanges();
+            {             
+                string SQL = "update MSTR_User set \n" +
+                             "UserName = '" + UserModel.User.UserName + "', \n" +
+                             "FirstName = '" + UserModel.User.FirstName + "', \n" +
+                             "MiddleName = '" + UserModel.User.MiddleName + "', \n" +
+                             "LastName = '" + UserModel.User.LastName + "', \n" +
+                             "MobileNo = '" + UserModel.User.MobileNo + "', \n" +
+                             "OfficeNo = '" + UserModel.User.OfficeNo + "', \n" +
+                             "HomeNo = '" + UserModel.User.HomeNo + "', \n" +
+                             "EmailId = '" + UserModel.User.EmailId + "', \n" +
+                             "CountryId = " + Util.toInt(UserModel.User.CountryId) + ", \n" +
+                             "PhotoUrl = '" + UserModel.User.PhotoUrl + "', \n" +
+                             "RPASPermitNo = '" + UserModel.User.RPASPermitNo + "', \n" +
+                             "PermitCategory = '" + UserModel.User.PermitCategory + "', \n" +
+                             "ContactAddress = '" + UserModel.User.ContactAddress + "', \n" +
+                             "RegRPASSerialNo = '" + UserModel.User.RegRPASSerialNo + "', \n" +
+                             "CompanyAddress = '" + UserModel.User.CompanyAddress + "', \n" +
+                             "CompanyTelephone = '" + UserModel.User.CompanyTelephone + "', \n" +
+                             "CompanyEmail = '" + UserModel.User.CompanyEmail + "', \n" +
+                             "EmiratesID = '" + UserModel.Pilot.EmiratesId + "' \n" +
+                             "where UserId =" + ID;                
+                int id = Util.doSQL(SQL);
+
+                string SQL1 = "update MSTR_User_Pilot set \n" +
+                            "PassportNo = '" + UserModel.Pilot.PassportNo + "',\n" +
+                            "DateOfExpiry = '" + UserModel.Pilot.DateOfExpiry + "',\n" +
+                            "EmiratesId = '" + UserModel.Pilot.EmiratesId + "'\n " +
+                            "where UserId = " + ID;
+                int Pid = Util.doSQL(SQL1);
+
+                //move the image to correct path
+                String UploadPath = Server.MapPath(Url.Content(RootUploadDir));
+                String newPath = UploadPath + id + "/";
+                String PhotoURL = UploadPath + "0/" + UserModel.User.PhotoUrl;
+                if (!System.IO.Directory.Exists(newPath)) Directory.CreateDirectory(newPath);
+                if (!String.IsNullOrEmpty(UserModel.User.PhotoUrl) &&
+                    System.IO.File.Exists(PhotoURL))
+                {
+                    System.IO.File.Move(PhotoURL, newPath + UserModel.User.PhotoUrl);
+                }
+
+                //to sent mail to the user created
+                //if (id != 0)
+                //{
+                //    var mailurl = "/Email/RPASUserCreated/" + id;
+                //    var mailsubject = "User has been created";
+                //    Util.EmailQue(Convert.ToInt32(Session["UserId"].ToString()), "info@exponent-ts.com", mailsubject, "~" + mailurl);
+
+                //    //need to update the RPAS Status after sending mail
+                //    SQL = "update mstr_rpas_user set [Status] = 'User Created' where rpasID = " + RPASID;
+                //    Util.doSQL(SQL);
+                //}
                 return RedirectToAction("Index");
             }
-            return View(mSTR_User);
+                var viewModel = new ViewModel.UserViewModel
+                {
+                    User = UserModel.User,
+                    Pilot = UserModel.Pilot,
+                    ProfileList = Util.GetProfileList(),
+                    CountryList = Util.GetCountryLists("Country", "CountryName", "Code", "sp"),
+                    AccountList = Util.GetAccountList(),
+                    DashboardList = Util.GetDashboardLists(),
+                    PermitCategoryList = Util.GetLists("RPASCategory")
+
+                };
+
+                return View(viewModel);                       
         }
 
         // GET: RpasUser/Delete/5
