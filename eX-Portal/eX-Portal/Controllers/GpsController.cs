90a5fa0026b99a3b220263075014dd4a1f969cba
ajithBhaskaran <ajithbhaskaranmail@gmail.com>
2016-06-23 13:24:31 +0400

    gps controller for gps log

---
+++ eX-Portal/eX-Portal/Controllers/GpsController.cs
@@ -0,0 +1,138 @@
+﻿using eX_Portal.exLogic;
+using eX_Portal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eX_Portal.Controllers
+{
+    public class GpsController : Controller
+    {
+
+        ExponentPortalEntities db = new ExponentPortalEntities();
+        // GET: Gps
+        public ActionResult Index()
+        {
+
+
+            ViewBag.Title = "Drone Service Listing";
+
+            String SQL = @"SELECT
+       [DroneId]
+      ,[DroneRFID]
+      ,[Latitude]
+      ,[Longitude]
+      ,[ProductRFID]
+      ,[ProductQrCode]
+      ,[ProductRSSI]
+      ,[ReadTime]
+      ,[CreatedTime]
+      ,[RecordType]
+      ,[IsActive]
+      ,[ProductId]
+      ,[Altitude]
+      ,[Speed]
+      ,[FixQuality]
+      ,[Satellites]
+      ,[Pitch]
+      ,[Roll]
+      ,[Heading]
+      ,[TotalFlightTime]
+      ,[BBFlightID]
+      ,[IsProcessed]      
+      ,[CreatedDate]
+      ,[Voltage],
+       Count(*) Over() as _TotalRecords,
+       [DroneGPSLogId] as _PKey
+       FROM[DroneGPSLog]";
+
+
+
+
+            qView nView = new qView(SQL);
+            if (Request.IsAjaxRequest())
+            {
+                Response.ContentType = "text/javascript";
+                return PartialView("qViewData", nView);
+            }
+            else
+            {
+                return View(nView);
+            }//if(IsAjaxRequest)
+        }
+
+        // GET: Gps/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Gps/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Gps/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Gps/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Gps/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Gps/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Gps/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
