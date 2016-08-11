e64b653fbfb95c25231981edd2aa0e3a3c0a7dd8
ajithBhaskaran <ajithbhaskaranmail@gmail.com>
2016-08-11 16:48:13 +0400

    recent chart changes

--- eX-Portal/eX-Portal/Scripts/RecentChart.js
+++ eX-Portal/eX-Portal/Scripts/RecentChart.js
@@ -57,8 +57,8 @@ function OnSuccess_(reponse) {
     {
         max = 15;
         scrollData= {
-            enabled: true
-           
+            enabled: true,
+            height:8
         }
 
     }
