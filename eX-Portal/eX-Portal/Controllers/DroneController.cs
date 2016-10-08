16eb2788251fb70f2ac2443905735fe119489d7e
roshanp4 <roshanp4@gmail.com>
2016-10-08 21:29:36 +0400

    drone page edited

--- eX-Portal/eX-Portal/Controllers/DroneController.cs
+++ eX-Portal/eX-Portal/Controllers/DroneController.cs
@@ -509,10 +509,10 @@ namespace eX_Portal.Controllers {
           "  D.[DroneSerialNo] as [UAS S.no],\n" +
           "  O.Name as OwnerName,\n" +
           "  M.Name as ManufactureName,\n" +
-          "  U.Name as UASType,\n" +
-          "  D.[DroneIdHexa] as UASHexaId,\n" +
-          "  D.[ModelName] as Description,\n" +
-          "  RegistrationAuthority as RegistrationAuthority\n" +
+          "  U.Name as UASType\n" +
+          //"  D.[DroneIdHexa] as UASHexaId,\n" +
+          //"  D.[ModelName] as Description,\n" +
+          //"  RegistrationAuthority as RegistrationAuthority\n" +
           "FROM\n" +
           "  [MSTR_Drone] D\n" +
           "Left join MSTR_Account  O on\n" +
