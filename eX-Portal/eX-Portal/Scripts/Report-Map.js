9c0e43ca6c8b05688cc05b7490387a5e87f36a12
sojanml <sojanml@gmail.com>
2017-08-23 13:16:55 +0400

    Fixed the error while generating the map without Approval.

--- eX-Portal/eX-Portal/Scripts/Report-Map.js
+++ eX-Portal/eX-Portal/Scripts/Report-Map.js
@@ -31,32 +31,34 @@ function initializeMap() {
   NoFlyZone = new google.maps.KmlLayer(KmlUrl, kmlOptions);
 
   // Construct the polygon.
-  var ApprovalInnerCoordinates = ToPath(ApprovalInnerPath);
-  var TempOuterPath = ToPath(ApprovalOuterPath);
-  var ApprovalOuterCoordinates = ApprovalInnerCoordinates.concat(ApprovalInnerCoordinates[0]);
-  ApprovalOuterCoordinates = ApprovalOuterCoordinates.concat(TempOuterPath);
+  if (ApprovalInnerPath != "" && ApprovalOuterPath != "" ) {
+    var ApprovalInnerCoordinates = ToPath(ApprovalInnerPath);
+    var TempOuterPath = ToPath(ApprovalOuterPath);
+    var ApprovalOuterCoordinates = ApprovalInnerCoordinates.concat(ApprovalInnerCoordinates[0]);
+    ApprovalOuterCoordinates = ApprovalOuterCoordinates.concat(TempOuterPath);
   
-  _ApprovalInnerPath = new google.maps.Polygon({
-    paths: ApprovalInnerCoordinates,
-    strokeColor: '#FF0000',
-    strokeOpacity: 0.8,
-    strokeWeight: 0,
-    fillColor: '#00FF00',
-    fillOpacity: 0.35,
-    map: map,
-    zIndex: 200
-  });
+    _ApprovalInnerPath = new google.maps.Polygon({
+      paths: ApprovalInnerCoordinates,
+      strokeColor: '#FF0000',
+      strokeOpacity: 0.8,
+      strokeWeight: 0,
+      fillColor: '#00FF00',
+      fillOpacity: 0.35,
+      map: map,
+      zIndex: 200
+    });
 
-  _ApprovalOuterPath = new google.maps.Polygon({
-    paths: ApprovalOuterCoordinates,
-    strokeColor: '#FF0000',
-    strokeOpacity: 0.8,
-    strokeWeight: 0,
-    fillColor: '#FF0000',
-    fillOpacity: 0.35,
-    map: map,
-    zIndex: 210
-  });
+    _ApprovalOuterPath = new google.maps.Polygon({
+      paths: ApprovalOuterCoordinates,
+      strokeColor: '#FF0000',
+      strokeOpacity: 0.8,
+      strokeWeight: 0,
+      fillColor: '#FF0000',
+      fillOpacity: 0.35,
+      map: map,
+      zIndex: 210
+    });
+  }
 
   _FlightReplayPath = new google.maps.Polyline({
     path: PolyPath,
@@ -75,8 +77,10 @@ function initializeMap() {
   for (var i = 0; i < PolyPath.length; i++) {
     bounds.extend(PolyPath[i]);
   }
-  for (var i = 0; i < ApprovalOuterCoordinates.length; i++) {
-    bounds.extend(ApprovalOuterCoordinates[i]);
+  if (ApprovalInnerPath != "" && ApprovalOuterPath != "") {
+    for (var i = 0; i < ApprovalOuterCoordinates.length; i++) {
+      bounds.extend(ApprovalOuterCoordinates[i]);
+    }
   }
   map.fitBounds(bounds);
   
